666ed48f1488a2c677ac56309de1f1c313f38665
NNASTYA2007 <advokat1707@gmail.com>
2022-01-31 22:40:15 +0200

    done trottl

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,11 @@
-const onPlay = function(data) {
+import throttle from 'lodash.throttle';
+
+const onPlay = function (data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
    
 };
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
  
 
 
@@ -23,3 +25,8 @@ player.setCurrentTime(time).then(function(seconds) {
     }
 });
 
+/**
+ * git add .
+git commit -m "description"
+git push
+ */
