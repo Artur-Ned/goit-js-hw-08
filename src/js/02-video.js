423c24b8b28af0680f1c1958617f7e473da86e40
NNASTYA2007 <advokat1707@gmail.com>
2022-02-02 23:34:15 +0200

    proba

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,17 @@
 import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+      player.on('play', function () {
+        console.log('played the video!');
+      });
+
+      player.getVideoTitle().then(function (title) {
+        console.log('title:', title);
+      });
+
 const onPlay = function (data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
    
@@ -9,10 +21,8 @@ player.on('timeupdate', throttle(onPlay, 1000));
  
 
 
-let time = localStorage.getItem("videoplayer-current-time");
-player.setCurrentTime(time).then(function(seconds) {
-    // seconds = localStorage.getItem("videoplayer-current-time");
-   
+const time = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(time) .then(function(seconds) {   
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
