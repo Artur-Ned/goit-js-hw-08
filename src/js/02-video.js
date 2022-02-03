90f9c5188af12124e991c8ac54125f2665e7a5c4
NNASTYA2007 <advokat1707@gmail.com>
2022-02-03 22:40:19 +0200

    зациклил сет в локал

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,9 +3,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-      player.on('play', function () {
-        console.log('played the video!');
-      });
+      player.on('play', onStart);
 
       player.getVideoTitle().then(function (title) {
         console.log('title:', title);
@@ -19,12 +17,17 @@ const onPlay = function (data) {
     player.off('timeupdate');
     localStorage.removeItem("videoplayer-current-time");
    };
+  
 }
 
 player.on('timeupdate', throttle(onPlay, 1000));
  const time = localStorage.getItem("videoplayer-current-time");
 const carentTime = time > null ? time : 0;
 player.setCurrentTime(carentTime); 
+
+
+
+function onStart() {player.on('timeupdate', throttle(onPlay, 1000))}; 
 
 // const time = localStorage.getItem("videoplayer-current-time") ?? 0;
  
