6e34c8b9b51acfae0d49222975f9843c22a0887f
NNASTYA2007 <advokat1707@gmail.com>
2022-02-03 21:19:35 +0200

    добавил тернарник с нуля начало

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,5 @@
 import throttle from 'lodash.throttle';
 
-
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
@@ -12,21 +11,27 @@ const player = new Vimeo.Player(iframe);
         console.log('title:', title);
       });
 
+
 const onPlay = function (data) {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
-   
-};
+  localStorage.setItem("videoplayer-current-time", data.seconds)
+  
+  if (data.seconds === data.duration) {
+    player.off('timeupdate');
+    localStorage.removeItem("videoplayer-current-time");
+   };
+}
 
 player.on('timeupdate', throttle(onPlay, 1000));
+ const time = localStorage.getItem("videoplayer-current-time");
+const carentTime = time > null ? time : 0;
+player.setCurrentTime(carentTime); 
+
+// const time = localStorage.getItem("videoplayer-current-time") ?? 0;
  
-
-
-const time = localStorage.getItem("videoplayer-current-time") ?? 0;
-
-player.setCurrentTime(time) 
-
+/**
 /**
  * git add .
 git commit -m "description"
 git push
  */
+ 
