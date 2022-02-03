0c787520fb9f0897d32b52500c69d1133ebe0582
NNASTYA2007 <advokat1707@gmail.com>
2022-02-03 23:47:40 +0200

    начал 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,6 @@
 
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('.email');
+const messageRef = document.querySelector('.message');
+const submitRef = document.querySelector('.submit');
+
