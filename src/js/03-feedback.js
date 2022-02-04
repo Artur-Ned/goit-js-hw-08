7a6e59f80c72eccc127be959079634f9913d7020
NNASTYA2007 <advokat1707@gmail.com>
2022-02-04 22:24:47 +0200

    переделать

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,71 @@
 
-const formRef = document.querySelector('.feedback-form');
-const emailRef = document.querySelector('.email');
-const messageRef = document.querySelector('.message');
-const submitRef = document.querySelector('.submit');
 
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const ref = {
+   form: document.querySelector('.feedback-form'),
+   textarea: document.querySelector('.submit')
+}
+
+ref.form.addEventListener('input', foo1);
+ref.form.addEventListener('submit', foo2);
+
+
+
+
+function foo1(event) {
+    console.log(event.target.value);
+
+ let formElenet = event.currentTarget.elements;
+    // let email = formElenet.email.value;
+    // let message = formElenet.message.value;
+      
+    
+    
+    console.log("hello");
+    
+    
+    
+    // let formData = {
+    //     email,
+    //     message
+    // }
+    
+    // localStorage.setItem("STORAGE_KEY", JSON.stringify(formData));
+
+
+
+}
+
+
+
+
+/**функция для формы */
+function foo2(event) {
+    event.preventDefault();
+     
+   
+    localStorage.removeItem("STORAGE_KEY");
+    console.log(formData);
+    event.target.reset();
+}
+
+/** let formElements = event.currentTarget.elements;
+    let email = formElements.email.value;
+    let password = formElements.password.value;
+    
+    let formData = {
+        email,
+        password
+    }
+    if (email === '' || password === '') {
+        alert('все поля должны быть заполнены')
+    } else console.log(formData);
+    
+     event.currentTarget.reset();
+     
+     eretet@ukr.net
+
+     
+     */
